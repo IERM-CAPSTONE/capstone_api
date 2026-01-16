f8ed80b08f8a4da53b1fbadb9f9ee081afe69303
TranDung147 <155507424+TranDung147@users.noreply.github.com>
2026-01-16 17:46:47 +0700

    Change prisma

---
+++ prisma/migrations/20260116100922_add_exam_schedule_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "ExamSession" ADD COLUMN     "examCode" TEXT,
+ADD COLUMN     "examType" TEXT,
+ADD COLUMN     "note" TEXT,
+ADD COLUMN     "openCode" TEXT,
+ADD COLUMN     "semester" TEXT;
